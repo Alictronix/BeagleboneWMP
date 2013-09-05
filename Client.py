62a344564e39e077bf8e88c8f6d4dc0f216989af
Alitronics <armjaz@gmail.com>
2013-09-05 14:22:40 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
-
-
 import socket
-
-
 
 host = '192.168.7.2'
 port = 50000
 size = 10
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
-
 
 while 1:
     yaw = s.recv(size)
@@ -23,8 +18,6 @@ while 1:
     y1=int(yaw)
     y2=int(pitch)
     y3=int(roll)
-    
-  
     
     print y1,y2,y3
    
